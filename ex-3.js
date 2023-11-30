97f4b84bf0ce4188bc6e3486106ab5db428e6947
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-11-30 15:37:09 +0700

    ex:finish loop-exercise

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,10 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
+let minNumber = numbers[0];
 // Start coding here
 
+for (let i = 0; i < numbers.length; i++) {
+  // console.log(numbers[i],minNumber);
+  if (numbers[i] < minNumber) minNumber = numbers[i];
+}
 console.log(minNumber);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+for (let i = companyName.length; i >= 0; i--) {
+  //console.log(i);
+  reversedCompanyName += companyName.charAt(i);
+}
 console.log(reversedCompanyName);
