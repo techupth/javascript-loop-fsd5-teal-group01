a8854d69da51cb95195f9f35e140203b404acc5f
Sodikcp <paramesh.rah@gmail.com>
2023-12-01 14:09:25 +0700

    feat:complete exercise loop

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,9 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+console.log(companyName.length-1)
+console.log(companyName[14])
+for (let i=companyName.length-1; i>= 0; i--) {
+    reversedCompanyName= reversedCompanyName+companyName[i]
+}
 console.log(reversedCompanyName);
